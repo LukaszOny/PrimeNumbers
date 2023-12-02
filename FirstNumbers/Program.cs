c08f47c9ba54da5aca874ffe8b147c23270eecd4
lonys <lonys@LAPTOP-8GOJV76L>
2023-12-02 17:35:07 +0100

    Add project files.

---
+++ FirstNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Hello");
+Console.WriteLine("Lets check prime numbers ");
+Console.WriteLine();
+
+int a = 0;
+int b = 0;
+
+while (b <= 100)
+{
+    int divisors = 0;
+    for (int d = 1; d <= b; d++)
+    {
+    if (b%d == 0)
+        {
+            divisors++;
+        }
+    }
+    if (divisors == 2)
+    {
+        a++;
+    }
+    b++;
+}
+Console.WriteLine($"you have got {a} Prime Numbers from 0 to 100. Have a nice day");
+
+   
+
+
+
+
+
